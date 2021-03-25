dbcf03f1a61f00466187ca7d90e9a956b685b441
mejamoleo <reonarudo11919273@gmail.com>
2021-03-25 10:40:30 +0900

    :recycle: 模範解答のコードが綺麗すぎ．
    アプローチとしては間違ってなかった．

--- problems/abc158_b.py
+++ problems/abc158_b.py
@@ -1,21 +1,6 @@
 N, A, B = map(int, input().split())
-unit = A + B
-count_u = 0
-count_all = 0
-
-if A == 0 or B == 0:
-	print(0)
-	exit()
-
-while N > count_all:
-	count_u += 1
-	count_all += unit
-
-# Nが青色の部分で終わる場合
-if N <= unit * (count_u - 1) + A:
-	ans = N - ((count_u - 1) * B)
-# Nが赤色の部分で終わる場合
-else:
-	ans = A * (count_u)
+ans = N // (A + B) * A
+rem = N % (A + B)
+ans += min(rem, A)
 
 print(ans)
