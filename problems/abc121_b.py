5a0e8a0660ba9a7a69015bf2bce3ee40956e5686
mejamoleo <reonarudo11919273@gmail.com>
2021-04-03 13:43:42 +0900

    :+1: vscodeのデバッグに救われた．
    20分ぐらい

---
+++ problems/abc121_b.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/abc121/tasks/abc121_b
+N,M,C = map(int, input().split())
+B = list(map(int, input().split()))
+
+ans = 0
+
+for i in range(N):
+	A = list(map(int, input().split()))
+	tmp = 0
+	for j in range(M):
+		tmp += A[j] * B[j]
+	if tmp + C > 0:
+		ans += 1
+print(ans)
