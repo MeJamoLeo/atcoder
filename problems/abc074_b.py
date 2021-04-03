e73f40931483be6b6252ee10323cfb1ef2c49e25
mejamoleo <reonarudo11919273@gmail.com>
2021-04-04 03:57:35 +0900

    "+1" easy 問題文が長いと億劫になりがち

---
+++ problems/abc074_b.py
@@ -0,0 +1,10 @@
+# https://atcoder.jp/contests/abc074/tasks/abc074_b
+N = int(input())
+K = int(input())
+X = list(map(int,input().split()))
+ans = 0
+
+for i in range(N):
+    ans += min(X[i], K-X[i])
+
+print(ans * 2) # x2 for return distance
