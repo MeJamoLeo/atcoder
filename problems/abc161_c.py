5a18f5c846715a9855fcafe562dfac241807c21c
mejamoleo <reonarudo11919273@gmail.com>
2021-04-04 22:16:04 +0900

    :recycle: 要復習

--- problems/abc161_c.py
+++ problems/abc161_c.py
@@ -1,22 +1,7 @@
 # https://atcoder.jp/contests/abc161/tasks/abc161_c
 
-import random
-N = random.randint(0,10**2)
-K = random.randint(1,10**2)
-count = 1
-print(N,K)
+N, K = map(int, input().split())
 
-# N, K = map(int, input().split())
-
-if K == 1:
-    print(0)
-    exit()
-
-sub = max(N,K) - min(N,K)
-while N > sub:
-    N = sub
-    sub = max(N,K) - min(N,K)
-    print(str(count)+":",N,K)
-    count += 1
-
-print(N)
+a = N % K
+ans = min(a, K - a)
+print(ans)
